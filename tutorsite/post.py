71f0fdc464c604f280c33f5d7d4dc00ff2859fc0
Ben <71541167+BenVN123@users.noreply.github.com>
2021-02-18 13:55:44 -0800

    ;)

---
+++ tutorsite/post.py
@@ -0,0 +1,45 @@
+from flask import Blueprint, flash, g, redirect, render_template, request, url_for
+from werkzeug.exceptions import abort
+from tutorsite.user import login_required
+from tutorsite.db import get_db
+
+post = Blueprint('post', __name__)
+
+@post.route('/')
+def index():
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('post/index.html', posts=posts)
+
+@post.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        db = get_db()
+        error = None
+
+        if len(title) < 4:
+            error = 'Title must be at least 4 characters.'
+        elif len(title) > 50:
+            error = 'Title must be at most 50 characters.'
+        elif len(body) < 10:
+            error = 'Body must be at least 10 characters.'
+
+        if error is None:
+            db.execute(
+                'INSERT INTO post (title, body, author_id) VALUES (?,?,?)',
+                (title, body, g.user['id'])
+            )
+            db.commit()
+
+            return redirect(url_for('post.index'))
+
+        flash(error)
+
+    return render_template('post/create.html')
